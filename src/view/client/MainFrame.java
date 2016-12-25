0ffa0d7b908eda7c38de6c88d2baea1a1724be45
ehzShelter <emrulhasanzawad@outlook.com>
2016-12-25 21:59:36 +0600

    SignIn Form is added with working login button from MainFrame

--- src/view/client/MainFrame.java
+++ src/view/client/MainFrame.java
@@ -1,86 +1,99 @@
 /*
-    This is the main frame of the client side application
-    All the
-*/
+   This is the main frame of the client side application
+   All the
+   */
 
-    package view.client;
-    import javax.swing.*;
-    import controller.*;
-    import view.signUpForm.SignUp;
-    import java.awt.event.ActionEvent;
-    import java.awt.event.ActionListener;
-
-
-    public class MainFrame{
-        JFrame frame;
-        JComboBox<String> productCombo;
-        JPanel categoryPanel;
-        JLabel heading;
-        JLabel label;
-        JLabel selected;
-        JButton login;
-        JButton register;
-
-        public MainFrame(){
-            this.frame = new JFrame("E-Commerce Desktop Application");
-            this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            this.frame.setBounds(300, 100, 800, 500);
-            this.frame.setLayout(null);
-
-            //JPanel for product category
-            this.categoryPanel = new JPanel();
-            this.heading = new JLabel("  Choose any product category from below and start shopping  ",JLabel.CENTER);
-            this.label = new JLabel("Product Category: ",JLabel.CENTER);
-            this.selected = new JLabel("Selected Category: --  Available Products: -- ",JLabel.CENTER);
-            this.productCombo = new JComboBox<String>();
-            this.productCombo.setBounds(20,10,700,200);
-
-            //login/signup buttons
-            this.login = new JButton("Login");
-            this.register = new JButton("Sign Up");
+package view.client;
+import javax.swing.*;
+import controller.*;
+import view.signUpForm.SignUp;
+import view.signInForm.SignIn;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 
-            register.addActionListener(new ActionListener() {
+public class MainFrame{
+    JFrame frame;
+    JComboBox<String> productCombo;
+    JPanel categoryPanel;
+    JLabel heading;
+    JLabel label;
+    JLabel selected;
+    JButton login;
+    JButton register;
+
+    public MainFrame(){
+        this.frame = new JFrame("E-Commerce Desktop Application");
+        this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.frame.setBounds(300, 100, 800, 500);
+        this.frame.setLayout(null);
+
+        //JPanel for product category
+        this.categoryPanel = new JPanel();
+        this.heading = new JLabel("  Choose any product category from below and start shopping  ",JLabel.CENTER);
+        this.label = new JLabel("Product Category: ",JLabel.CENTER);
+        this.selected = new JLabel("Selected Category: --  Available Products: -- ",JLabel.CENTER);
+        this.productCombo = new JComboBox<String>();
+        this.productCombo.setBounds(20,10,700,200);
+
+        //login/signup buttons
+        this.login = new JButton("Login");
+
+
+        login.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                loginBtnActionPerformed(evt);
+            }
+        });
+
+        this.register = new JButton("Sign Up");
+
+
+        register.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent evt) {
                 registerBtnActionPerformed(evt);
             }
         });
 
 
-            //add category into combo box
-            this.productCombo.addItem(" -- ");
-            this.productCombo.addItem("Electronics");
-            this.productCombo.addItem("Books");
-            this.productCombo.addItem("Cars");
-            this.productCombo.addItem("Fashion");
-            this.productCombo.addItem("Sports");
-            this.productCombo.addItem("Movies");
-            this.productCombo.addItem("Music");
+        //add category into combo box
+        this.productCombo.addItem(" -- ");
+        this.productCombo.addItem("Electronics");
+        this.productCombo.addItem("Books");
+        this.productCombo.addItem("Cars");
+        this.productCombo.addItem("Fashion");
+        this.productCombo.addItem("Sports");
+        this.productCombo.addItem("Movies");
+        this.productCombo.addItem("Music");
 
-            this.categoryPanel.setBounds(150,10,400,200);
-            this.categoryPanel.add(this.login);
-            this.categoryPanel.add(this.register);
-            this.categoryPanel.add(heading);
-            this.categoryPanel.add(label);
-            this.categoryPanel.add(this.productCombo);
-            this.categoryPanel.add(this.selected);
+        this.categoryPanel.setBounds(150,10,400,200);
+        this.categoryPanel.add(this.login);
+        this.categoryPanel.add(this.register);
+        this.categoryPanel.add(heading);
+        this.categoryPanel.add(label);
+        this.categoryPanel.add(this.productCombo);
+        this.categoryPanel.add(this.selected);
 
-            //registering event handler of the JComboBox
-            CategoryController categoryController = new CategoryController(this.productCombo,this.categoryPanel,this.selected);
-            this.productCombo.addActionListener(categoryController);
+        //registering event handler of the JComboBox
+        CategoryController categoryController = new CategoryController(this.productCombo,this.categoryPanel,this.selected);
+        this.productCombo.addActionListener(categoryController);
 
-            this.frame.add(categoryPanel);
-            this.frame.setVisible(true);
-        }
+        this.frame.add(categoryPanel);
+        this.frame.setVisible(true);
+    }
 
-      private void registerBtnActionPerformed(ActionEvent evt) {
+    private void registerBtnActionPerformed(ActionEvent evt) {
 
         SignUp sampleSignUp = new SignUp("Sign Up Form");
-       // System.out.println("Successful register");
     }
 
-        void setSelectedText(){
+    private void loginBtnActionPerformed(ActionEvent evt) {
 
-        }
+        SignIn sampleSignIn = new SignIn("Sign In Form");
+    }
+
+    void setSelectedText(){
 
     }
+
+}
---
+++ src/view/signInForm/SignIn.java
@@ -0,0 +1,107 @@
+package view.signInForm;
+
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JTextField;
+import javax.swing.WindowConstants;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SignIn extends JFrame {
+
+    private JLabel email;
+    private JTextField emailField;
+    private JLabel password;
+    private JPasswordField passwordField;
+    private JComboBox<String> personSwitchCombo;
+    private JButton signIn;
+
+    private String title;
+
+    public SignIn() {}
+
+    public SignIn(String title) {
+        super(title);
+        this.email = new JLabel();
+        this.password = new JLabel();
+        this.personSwitchCombo = new JComboBox<>();
+        this.signIn = new JButton();
+        this.emailField = new JTextField();
+        this.passwordField = new JPasswordField();
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        this.email.setText("email");
+
+        this.password.setText("password");
+
+        this.personSwitchCombo.setModel(new DefaultComboBoxModel<String>(new String[] { "client", "admin" }));
+
+        this.signIn.setText("SignIn");
+        this.signIn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                signInActionPerformed(evt);
+            }
+        });
+
+
+        GroupLayout layout = new GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+
+        // horizontal
+        layout.setHorizontalGroup(
+                layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addGap(85, 85, 85)
+                    .addGroup(layout.createParallelGroup(GroupLayout.Alignment.TRAILING)
+                        .addGroup(layout.createSequentialGroup()
+                            .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING, false)
+                                .addComponent(email, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(password, GroupLayout.DEFAULT_SIZE, 110, Short.MAX_VALUE))
+                            .addGap(94, 94, 94)
+                            .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                                .addComponent(emailField, GroupLayout.PREFERRED_SIZE, 333, GroupLayout.PREFERRED_SIZE)
+                                .addComponent(passwordField, GroupLayout.PREFERRED_SIZE, 333, GroupLayout.PREFERRED_SIZE)))
+                        .addGroup(layout.createSequentialGroup()
+                            .addComponent(personSwitchCombo, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                            .addGap(466, 466, 466))
+                        .addComponent(signIn, GroupLayout.PREFERRED_SIZE, 132, GroupLayout.PREFERRED_SIZE))
+                    .addContainerGap(140, Short.MAX_VALUE))
+                );
+
+        // Vertical
+        layout.setVerticalGroup(
+                layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addGap(144, 144, 144)
+                    .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                        .addComponent(email, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
+                        .addComponent(emailField, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE))
+                    .addGap(44, 44, 44)
+                    .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                        .addComponent(password, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
+                        .addComponent(passwordField, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE))
+                    .addGap(77, 77, 77)
+                    .addComponent(personSwitchCombo, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                    .addPreferredGap(ComponentPlacement.UNRELATED)
+                    .addComponent(signIn, GroupLayout.PREFERRED_SIZE, 44, GroupLayout.PREFERRED_SIZE)
+                    .addContainerGap(82, Short.MAX_VALUE))
+                );
+
+        this.setVisible(true);
+        this.pack();
+    }
+
+
+    private void signInActionPerformed(ActionEvent evt) {
+        // TODO add your handling code here:
+    }
+}
