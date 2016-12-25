d8f9ae52ac2bb6717f9e39286a7698d3cec1aea8
ehzShelter <emrulhasanzawad@outlook.com>
2016-12-25 18:48:57 +0600

    Register form added and button register working also

--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -1,15 +1,17 @@
 /*
-	Load the main from here
-	the "MainFrame" will be loaded
+    Load the main from here
+    the "MainFrame" will be loaded
 */
-	package main;
+    package main;
 
-	import view.client.*;
+    import view.client.*;
+    import view.signUpForm.SignUp;
     import db.DBConnectionProvider;
     import db.DBDataProvider;
 
-	public class MainClass{
-		public static void main(String args[]){
-			MainFrame window = new MainFrame();
-		}
-	}
+    public class MainClass{
+        public static void main(String args[]){
+            MainFrame window = new MainFrame();
+            // SignUp sampleSignUp = new SignUp("Sign Up Form");
+        }
+    }
--- src/view/client/MainFrame.java
+++ src/view/client/MainFrame.java
@@ -1,69 +1,86 @@
 /*
-	This is the main frame of the client side application
-	All the 
+    This is the main frame of the client side application
+    All the
 */
 
-	package view.client;
-	import javax.swing.*;
-	import controller.*;
+    package view.client;
+    import javax.swing.*;
+    import controller.*;
+    import view.signUpForm.SignUp;
+    import java.awt.event.ActionEvent;
+    import java.awt.event.ActionListener;
 
 
-	public class MainFrame{
-		JFrame frame;
-		JComboBox<String> productCombo;
-		JPanel categoryPanel;
-		JLabel heading;
-		JLabel label;
-		JLabel selected;
-		JButton login;
-		JButton register;
+    public class MainFrame{
+        JFrame frame;
+        JComboBox<String> productCombo;
+        JPanel categoryPanel;
+        JLabel heading;
+        JLabel label;
+        JLabel selected;
+        JButton login;
+        JButton register;
 
-		public MainFrame(){
-			this.frame = new JFrame("E-Commerce Desktop Application");
-			this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			this.frame.setBounds(300, 100, 800, 500);
-			this.frame.setLayout(null);
+        public MainFrame(){
+            this.frame = new JFrame("E-Commerce Desktop Application");
+            this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            this.frame.setBounds(300, 100, 800, 500);
+            this.frame.setLayout(null);
 
-			//JPanel for product category
-			this.categoryPanel = new JPanel();
-			this.heading = new JLabel("  Choose any product category from below and start shopping  ",JLabel.CENTER);
-			this.label = new JLabel("Product Category: ",JLabel.CENTER); 
-			this.selected = new JLabel("Selected Category: --  Available Products: -- ",JLabel.CENTER); 
-			this.productCombo = new JComboBox<String>();
-			this.productCombo.setBounds(20,10,700,200);
+            //JPanel for product category
+            this.categoryPanel = new JPanel();
+            this.heading = new JLabel("  Choose any product category from below and start shopping  ",JLabel.CENTER);
+            this.label = new JLabel("Product Category: ",JLabel.CENTER);
+            this.selected = new JLabel("Selected Category: --  Available Products: -- ",JLabel.CENTER);
+            this.productCombo = new JComboBox<String>();
+            this.productCombo.setBounds(20,10,700,200);
 
-			//login/signup buttons
-			this.login = new JButton("Login");
-			this.register = new JButton("Sign Up");
+            //login/signup buttons
+            this.login = new JButton("Login");
+            this.register = new JButton("Sign Up");
 
-			//add category into combo box
-			this.productCombo.addItem(" -- ");
-			this.productCombo.addItem("Electronics");
-			this.productCombo.addItem("Books");
-			this.productCombo.addItem("Cars");
-			this.productCombo.addItem("Fashion");
-			this.productCombo.addItem("Sports");
-			this.productCombo.addItem("Movies");
-			this.productCombo.addItem("Music");
 
-			this.categoryPanel.setBounds(150,10,400,200);
-			this.categoryPanel.add(this.login);
-			this.categoryPanel.add(this.register);
-			this.categoryPanel.add(heading);
-			this.categoryPanel.add(label);
-			this.categoryPanel.add(this.productCombo);
-			this.categoryPanel.add(this.selected);
+            register.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                registerBtnActionPerformed(evt);
+            }
+        });
 
-			//registering event handler of the JComboBox
-			CategoryController categoryController = new CategoryController(this.productCombo,this.categoryPanel,this.selected);
-			this.productCombo.addActionListener(categoryController);
 
-			this.frame.add(categoryPanel);
-			this.frame.setVisible(true);
-		}
+            //add category into combo box
+            this.productCombo.addItem(" -- ");
+            this.productCombo.addItem("Electronics");
+            this.productCombo.addItem("Books");
+            this.productCombo.addItem("Cars");
+            this.productCombo.addItem("Fashion");
+            this.productCombo.addItem("Sports");
+            this.productCombo.addItem("Movies");
+            this.productCombo.addItem("Music");
 
-		void setSelectedText(){
+            this.categoryPanel.setBounds(150,10,400,200);
+            this.categoryPanel.add(this.login);
+            this.categoryPanel.add(this.register);
+            this.categoryPanel.add(heading);
+            this.categoryPanel.add(label);
+            this.categoryPanel.add(this.productCombo);
+            this.categoryPanel.add(this.selected);
 
-		}
+            //registering event handler of the JComboBox
+            CategoryController categoryController = new CategoryController(this.productCombo,this.categoryPanel,this.selected);
+            this.productCombo.addActionListener(categoryController);
 
-	}
+            this.frame.add(categoryPanel);
+            this.frame.setVisible(true);
+        }
+
+      private void registerBtnActionPerformed(ActionEvent evt) {
+
+        SignUp sampleSignUp = new SignUp("Sign Up Form");
+       // System.out.println("Successful register");
+    }
+
+        void setSelectedText(){
+
+        }
+
+    }
