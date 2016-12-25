e2e6d730f02ac39973eccd692357fb966adb6f04
ehzShelter <emrulhasanzawad@outlook.com>
2016-12-25 17:29:08 +0600

    DB password root

--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -1,10 +1,12 @@
 /*
 	Load the main from here
-	the "MainFrame" will be loaded 
+	the "MainFrame" will be loaded
 */
 	package main;
 
 	import view.client.*;
+    import db.DBConnectionProvider;
+    import db.DBDataProvider;
 
 	public class MainClass{
 		public static void main(String args[]){
