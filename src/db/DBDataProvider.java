f0c8b2b9fb9b8b1c0a1cf213a4b0657b71850313
abrarShariar <shariarabrar@gmail.com>
2016-12-28 01:50:55 +0600

    trying category

--- src/db/DBDataProvider.java
+++ src/db/DBDataProvider.java
@@ -54,7 +54,6 @@
         v1.addElement("QUANTITY");
 
         try{
-
           while(res.next()){
             Vector v2 = new Vector();
             v2.addElement(res.getString("name"));
@@ -64,7 +63,6 @@
             v3.add(v2);
           }
           
-          
         }catch(Exception e){
           System.out.println("ERROR: " + e);
         }
@@ -72,9 +70,56 @@
        System.out.println(ex.getMessage());
      }
 
-        // System.out.println(v3);
      productTable = new JTable(v3,v1);
      return productTable;
    }
 
+
+
+  //get specific product
+   public static JTable getSpecificProduct(String category){
+
+    JTable productTable;
+    Vector v1 = new Vector();
+    Vector v3 = new Vector();
+    try {
+            //Get the conn from DBConnectionProvider
+      Connection conn = DBConnectionProvider.getDBConnection();
+            //Create Statement to execute query
+      PreparedStatement statement = conn.prepareStatement("SELECT * FROM ecommercejava.product WHERE product.category = ?");
+      statement.setString(1, category);
+      ResultSet res = statement.executeQuery();
+
+      v1.addElement("NAME");
+      v1.addElement("CATEGORY");
+      v1.addElement("PRICE");
+      v1.addElement("QUANTITY");
+
+      try{
+
+        while(res.next()){
+          Vector v2 = new Vector();
+          v2.addElement(res.getString("name"));
+          v2.addElement(res.getString("category"));
+          v2.addElement(Double.toString(res.getDouble("price")));
+          v2.addElement(Integer.toString(res.getInt("quantity")));
+          v3.add(v2);
+        }
+
+
+      }catch(Exception e){
+        System.out.println("ERROR: " + e);
+      }
+    } catch (Exception ex) {
+     System.out.println(ex.getMessage());
+   }
+
+        // System.out.println(v3);
+   productTable = new JTable(v3,v1);
+   return productTable;
  }
+
+
+
+
+}
